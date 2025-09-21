f0de61c875a02e8a0b5cf4c0c6cec4d37ebf244e
Ivgeny Tokarzhevsky <2000jenya@gmail.com>
2025-09-22 00:49:38 +0300

    manifest added

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ function toggleLed() {
   const status = document.getElementById("led-status");
   status.textContent = ledOn ? "ON" : "OFF";
   status.style.color = ledOn ? "green" : "red";
+  status.style.fontWeight = "bold";
 }
